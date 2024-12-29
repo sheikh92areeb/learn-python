611cddc64e9d48579896e2665388f49da437408e
Muhammad Areeb <mareebsheikh92@gmail.com>
2024-12-30 00:44:52 +0500

    update lesson 007

---
+++ Lesson-007/main.py
@@ -0,0 +1,35 @@
+# we can calculate two numbers with the help of arthmatic operators
+# +, -, *, /, %, **, //
+
+num1 = 15
+num2 = 8
+print("Number 1: ", num1)
+print("Number 2: ", num2)
+
+# addition
+result = num1 + num2
+print("Addition: ", result)
+
+# subtraction
+result = num1 - num2
+print("Subtraction: ", result)
+
+# multiplication
+result = num1 * num2
+print("Multiplication: ", result)
+
+# division
+result = num1 / num2
+print("Division: ", result)
+
+# modulus
+result = num1 % num2
+print("Modulus: ", result)
+
+# exponent
+result = num1 ** num2
+print("Exponent: ", result)
+
+# floor division
+result = num1 // num2
+print("Floor Division: ", result)
