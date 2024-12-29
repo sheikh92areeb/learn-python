664b5507862358407cafdd8070f67fb2417ece98
Muhammad Areeb <mareebsheikh92@gmail.com>
2024-12-30 00:18:48 +0500

    Update lesson 006

---
+++ Lesson-006/main.py
@@ -0,0 +1,63 @@
+# these are variable examples
+a = 1
+print(a)
+b = "Ali"
+print(b)
+c = True
+print(c)    
+d = None
+print(d)
+
+# we can check the type of variable by using type() function
+print("The type of a is",type(a))
+print("The type of b is",type(b))
+print("The type of c is",type(c))
+print("The type of d is",type(d))
+
+# python provides us built-in data types
+# 1- Numbers (int, float, complex)
+
+# int
+int = 1
+print(int)
+print("The type of int is", type(int))
+
+# float
+float = 1.0
+print(float)
+print("The type of float is", type(float))
+
+# complex number (a+bj)
+complex = 1+2j
+print(complex)
+print("The type of complex is", type(complex)) 
+
+# 2- String
+
+str = "Hello, World!"
+print(str) 
+print("The type of str is", type(str))
+
+# 3- Boolean
+
+bool = True
+print(bool)
+print("The type of bool is", type(bool))
+
+# 4- List
+
+list1 = [1, 2, [3, 4], 5]
+print(list1)
+print("The type of list1 is", type(list1))
+
+# 5- Tuple
+
+tuple1 = (1, 2, [3, 4], 5)
+print(tuple1)  
+print("The type of tuple1 is", type(tuple1))
+
+# 6- Dictionary
+
+dict1 = {"name": "Ali", "age": 25} 
+print(dict1)
+print("The type of dict1 is", type(dict1))
