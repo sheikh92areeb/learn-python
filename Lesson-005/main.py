fadc3f2b3440b01bce4b69479508a5c6730cf16a
Muhammad Areeb <mareebsheikh92@gmail.com>
2024-12-29 23:38:18 +0500

    update lesson 005

---
+++ Lesson-005/main.py
@@ -0,0 +1,37 @@
+# This is a single line comment
+print("Hello, World!")
+
+print("Hello, World!") # we can write a comment after the code on same line
+
+print("Hello, World!") 
+# we can write a comment after the next line of code
+
+
+# This is a multi-line comment
+# we can write multiple lines of comments
+# using this method
+print("Hello, World!")
+
+"""
+This is a multi-line comment
+we can write multiple lines of comments
+using this method
+"""
+print("Hello, World!")
+
+
+# This is Escape Sequence Character of new line
+print("I am a Python program.\nand this learner is a Python learner.")
+
+# This is Escape Sequence Character of qoutation mark
+print("I am a \"Python program\".")
+
+# we can use multiple values in print statement
+print("Hello", "World", "Python", "Learner")
+
+# we can use multiple values in print statement with separator
+print("Hello", "World", "Python", "Learner", sep=", ")
+
+# we can use multiple values in print statement with separator and end
+print("Hello", "World", "Python", "Learner", sep=", ", end="!\n")
+print("it's a new line")
