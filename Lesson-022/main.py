7bfda93441b8add2539d44c4fd01af32e33101a1
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-01-09 22:48:08 +0500

    update lesson 022

---
+++ Lesson-022/main.py
@@ -0,0 +1,13 @@
+marks = [55,66,83,99,22,53,47,84,55,13]
+
+# print(marks)
+# print(type(marks))
+# print(marks[0])
+# print(marks[1])
+# print(marks[2])
+
+print(marks)
+
+# we can use slicing as string
+print(marks[:])
+print(marks[1:7:2])
