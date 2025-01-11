1b852e9dc751ce436ae2332c04c47c600f181d60
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-01-11 14:19:52 +0500

    Create main.py

---
+++ Lesson-028/main.py
@@ -0,0 +1,11 @@
+letter = "Hey I am {}, and I am from {}."
+name = "Areeb"
+country = "Pakistan"
+
+print(letter.format(name , country))
+
+
+# we can use f-string for better experience
+
+letter1 = f"Hey I am {name}, and I am from {country}."
+print(letter1)
