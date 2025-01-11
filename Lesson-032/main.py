7f85da90ffb498012609929aecb74ba133bd7901
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-01-11 23:18:10 +0500

    Create main.py

---
+++ Lesson-032/main.py
@@ -0,0 +1,8 @@
+cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+cities2 = {"Tokyo", "Seoul", "Kabul", "Madrid"}
+
+cities3 = cities.union(cities2)
+print(cities3)
+
+cities3 = cities.intersection(cities2)
+print(cities3)
