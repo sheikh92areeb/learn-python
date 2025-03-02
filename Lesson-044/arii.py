58a69141acb4129db16819b91b45ab9d834eaf5f
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-03-02 23:37:19 +0500

    update

--- Lesson-044/arii.py
+++ Lesson-044/arii.py
@@ -2,4 +2,5 @@ def welcome():
     print("Welcome to the world of python")
 
 
-welcome()    
+if __name__ == "__main__":
+    welcome()    
--- Lesson-044/main.py
+++ Lesson-044/main.py
@@ -1,4 +1,4 @@
 import arii 
 
-if __name__ == "__main__":
-    arii.welcome()
+
+arii.welcome()
